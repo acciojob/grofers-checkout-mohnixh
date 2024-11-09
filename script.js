a010b70c19c5a8b8313b6f7ec3e8714a97c37563
mohnixh <153740400+mohnixh@users.noreply.github.com>
2024-11-09 11:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let arr = Array.from(document.querySelectorAll(".price"));
+	let sum = 0;
+	arr.forEach((item)=> {
+		 sum += parseInt(item.innerText);
+	});
+	let row =document.createElement("tr");
+	row.setAttribute('id',"ans");
+	let cell1 = document.createElement("td");
+	cell1.innerText = "Total is: ";
+	let cell2 = document.createElement("td");
+	cell2.innerText = sum;
+	row.appendChild(cell1)
+	row.appendChild(cell2);
+	table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
